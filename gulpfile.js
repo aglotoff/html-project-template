00944a24752f5c48f62be299cb5ca75b30c84a62
Andrey Glotov <andrei.glotoff@gmail.com>
2018-08-14 11:30:13 +0300

    update module list in gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -38,27 +38,37 @@
 //   Modules
 // ----------------------------------------
 //
-// browser-sync        : Keep multiple browsers & devices in sync
-// browserify          : require('modules') in the browser
-// del                 : Delete files and folders using globs
-// gulp                : The streaming build system
-// gulp-babel          : Use next generation JavaScript, today, with Babel
-// gulp-eslint         : A gulp plugin for ESLint
-// gulp-html-beautify  : A gulp plugin to beautify HTML files
-// gulp-imagemin       : Minify PNG, JPEG, GIF and SVG images with imagemin
-// gulp-load-plugins   : Loads gulp plugins and attaches them to an object
-// gulp-plumber        : Prevent pipe breaking caused by errors from plugins
-// gulp-pug            : Gulp plugin for compiling Pug templates
-// gulp-sass           : Sass plugin for Gulp
-// gulp-sourcemaps     : Write inline source maps
-// gulp-stylelint      : Run stylelint results through a list of reporters
-// gulp-svg-sprite     : Create SVG sprites
-// gulp-wait           : Insert a delay before calling the next function
-// gulp-watch          : File watcher
-// minimist            : Parse argument options
-// require-dir         : Helper to require() directories
-// vinyl-buffer        : Convert streaming vinyl files to use buffers
-// vinyl-source-stream : Text streams at the start of gulp or vinyl pipelines
+// autoprefixer         : Parse CSS and add vendor prefixes to CSS rules
+// babel-core           : Babel compiler core
+// babel-preset-env     : A Babel preset for each environment
+// babel-preset-stage-0 : Babel preset for stage 0 plugins
+// babelify             : Babel browserify transform
+// browser-sync         : Keep multiple browsers & devices in sync
+// browserify           : require('modules') in the browser
+// cssnano              : A modular minifier, built on top of PostCSS
+// del                  : Delete files and folders using globs
+// gulp                 : The streaming build system
+// gulp-eslint          : A gulp plugin for ESLint
+// gulp-html-beautify   : A gulp plugin to beautify HTML files
+// gulp-if              : Conditionally run a task
+// gulp-imagemin        : Minify PNG, JPEG, GIF and SVG images with imagemin
+// gulp-load-plugins    : Loads gulp plugins and attaches them to an object
+// gulp-plumber         : Prevent pipe breaking caused by errors from plugins
+// gulp-postcss         : PostCSS gulp plugin
+// gulp-pug             : Gulp plugin for compiling Pug templates
+// gulp-sass            : Sass plugin for Gulp
+// gulp-sourcemaps      : Write inline source maps
+// gulp-stylelint       : Run stylelint results through a list of reporters
+// gulp-svg-sprite      : Create SVG sprites
+// gulp-uglify          : Minify files with UglifyJS
+// gulp-wait            : Insert a delay before calling the next function
+// gulp-watch           : File watcher
+// minimist             : Parse argument options
+// require-dir          : Helper to require() directories
+// stylelint            : A mighty, modern CSS linter
+// stylelint-scss       : A collection of SCSS specific rules for stylelint
+// vinyl-buffer         : Convert streaming vinyl files to use buffers
+// vinyl-source-stream  : Text streams at the start of gulp or vinyl pipelines
 //
 // ----------------------------------------
 
