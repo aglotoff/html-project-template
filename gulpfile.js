22d2e4fb910750b2f9f648f82ec7ac22f372ea43
Andrey Glotov <andrei.glotoff@gmail.com>
2018-09-15 15:07:07 +0300

    update plugins list

--- gulpfile.js
+++ gulpfile.js
@@ -38,39 +38,36 @@
 //   Modules
 // ----------------------------------------
 //
-// autoprefixer         : Parse CSS and add vendor prefixes to CSS rules
-// babel-core           : Babel compiler core
-// babel-preset-env     : A Babel preset for each environment
-// babel-preset-stage-0 : Babel preset for stage 0 plugins
-// babelify             : Babel browserify transform
-// browser-sync         : Keep multiple browsers & devices in sync
-// browserify           : require('modules') in the browser
-// cssnano              : A modular minifier, built on top of PostCSS
-// del                  : Delete files and folders using globs
-// gulp                 : The streaming build system
-// gulp-concat          : Concatenates files
-// gulp-eslint          : A gulp plugin for ESLint
-// gulp-html-beautify   : A gulp plugin to beautify HTML files
-// gulp-if              : Conditionally run a task
-// gulp-imagemin        : Minify PNG, JPEG, GIF and SVG images with imagemin
-// gulp-plumber         : Prevent pipe breaking caused by errors from plugins
-// gulp-postcss         : PostCSS gulp plugin
-// gulp-pug             : Gulp plugin for compiling Pug templates
-// gulp-sass            : Sass plugin for Gulp
-// gulp-sourcemaps      : Write inline source maps
-// gulp-stylelint       : Run stylelint results through a list of reporters
-// gulp-svg-sprite      : Create SVG sprites
-// gulp-uglify          : Minify files with UglifyJS
-// gulp-wait            : Insert a delay before calling the next function
-// gulp-watch           : File watcher
-// imagemin-mozjpeg     : Imagemin plugin for mozjpeg
-// merge-stream         : Merge (interleave) a bunch of streams
-// minimist             : Parse argument options
-// require-dir          : Helper to require() directories
-// stylelint            : A mighty, modern CSS linter
-// stylelint-scss       : A collection of SCSS specific rules for stylelint
-// vinyl-buffer         : Convert streaming vinyl files to use buffers
-// vinyl-source-stream  : Text streams at the start of gulp or vinyl pipelines
+// @babel/core        : Babel compiler core
+// @babel/preset-env  : A Babel preset for each environment
+// autoprefixer       : Parse CSS and add vendor prefixes to CSS rules
+// babel-loader       : transpiling JavaScript files using Babel and webpack
+// browser-sync       : Keep multiple browsers & devices in sync
+// cssnano            : A modular minifier, built on top of PostCSS
+// del                : Delete files and folders using globs
+// gulp               : The streaming build system
+// gulp-changed       : Only pass through changed files
+// gulp-data          : Generate a data object for other plugins to consume
+// gulp-eslint        : A gulp plugin for ESLint
+// gulp-html-beautify : A gulp plugin to beautify HTML files
+// gulp-imagemin      : Minify PNG, JPEG, GIF and SVG images with imagemin
+// gulp-plumber       : Prevent pipe breaking caused by errors from plugins
+// gulp-postcss       : PostCSS gulp plugin
+// gulp-pug           : Gulp plugin for compiling Pug templates
+// gulp-sass          : Sass plugin for Gulp
+// gulp-sourcemaps    : Write inline source maps
+// gulp-stylelint     : Run stylelint results through a list of reporters
+// gulp-svg-sprite    : Create SVG sprites
+// gulp-wait          : Insert a delay before calling the next function
+// gulp-watch         : File watcher
+// imagemin-mozjpeg   : Imagemin plugin for mozjpeg
+// minimist           : Parse argument options
+// require-dir        : Helper to require() directories
+// run-sequence       : Runs a sequence of gulp tasks in the specified order
+// stylelint          : A mighty, modern CSS linter
+// stylelint-scss     : A collection of SCSS specific rules for stylelint
+// webpack            : A module bundler
+// webpack-stream     : Run webpack as a stream
 //
 // ----------------------------------------
 
