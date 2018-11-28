09573d599c111b586f1a4cc113f3c4cbe0f99daf
Andrey Glotov <andrei.glotoff@gmail.com>
2018-11-28 18:50:20 +0300

    fix webpack crashing

--- gulp/tasks/js.js
+++ gulp/tasks/js.js
@@ -1,23 +1,10 @@
 const browserSync = require('browser-sync');
 const del = require('del');
 const gulp = require('gulp');
-const plumber = require('gulp-plumber');
-const lazypipe = require('lazypipe');
 const named = require('vinyl-named');
 const webpack = require('webpack-stream');
 
 const config = require('../config');
-
-let watching = false;
-
-const jsTasks = lazypipe()
-    .pipe(plumber)
-    .pipe(named)
-    .pipe(() => webpack({
-        ...config.plugins.webpack,
-        watch: watching,
-    }))
-    .pipe(gulp.dest, config.paths.js.dest);
 
 // ----------------------------------------
 //   Task: Build: JavaScript
@@ -25,7 +12,12 @@ const jsTasks = lazypipe()
 
 gulp.task('build:js', () => {
     return gulp.src(config.paths.js.src)
-        .pipe(jsTasks());
+        .pipe(named())
+        .pipe(webpack(config.plugins.webpack))
+        .on('error', function() {
+            this.emit('end');
+        })
+        .pipe(gulp.dest(config.paths.js.dest));
 });
 
 // ----------------------------------------
@@ -35,7 +27,15 @@ gulp.task('build:js', () => {
 gulp.task('watch:js', () => {
     watching = true;
     return gulp.src(config.paths.js.src)
-        .pipe(jsTasks())
+        .pipe(named())
+        .pipe(webpack({
+            ...config.plugins.webpack,
+            watch: true,
+        }))
+        .on('error', function() {
+            this.emit('end');
+        })
+        .pipe(gulp.dest(config.paths.js.dest))
         .pipe(browserSync.reload({stream: true}));
 });
 
